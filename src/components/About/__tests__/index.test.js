1521eb3ebd286802aaddc0fa5595172ca70aaf7d
zue <zuleikatesei@gmail.com>
2022-11-07 19:56:07 -0800

    about component tests pass

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+describe('About component', () => {
+    // First Test
+    it('renders', () => {
+        render(<About />);
+    });
+
+    // Second Test
+    it('matches snaapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+    })
+})
