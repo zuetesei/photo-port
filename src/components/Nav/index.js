fecba834d7548efa61d289222927f25e21cfb238
zue <zuleikatesei@gmail.com>
2022-11-08 17:17:06 -0800

    displayed the photo gallery

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
@@ -9,9 +9,14 @@ function Nav(props) {
         currentCategory,
     } = props;
 
-    useEffect(() => {
-        document.title = capitalizeFirstLetter(currentCategory.name);
-    }, [currentCategory]);
+    const handleClick = (item) => {
+        console.log(item);
+        return item;
+    }
+
+    // useEffect(() => {
+    //     document.title = capitalizeFirstLetter(currentCategory.name);
+    // }, [currentCategory]);
 
     return (
         <header className="flex-row px-1">
@@ -23,14 +28,19 @@ function Nav(props) {
             <nav>
                 <ul className="flex-row">
                     <li className='mx-2'>
-                        <a href="#about"> About Me</a>
+                        <a data-testid="about" href="#about"> About Me</a>
                     </li>
-                    <li>
-                        <span> Contact </span>
+                    <li className={'mx-2'}>
+                        <span onClick={() => handleClick('Contact')}> Contact </span>
                     </li>
                     {categories.map((category) => (
-                        <li className={`mx-1' ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
-                            <span onClick={() => { setCurrentCategory(category) }} >
+                        <li className={`mx-1' ${currentCategory.name === category.name}`}
+                            key={category.name}
+                        >
+                            <span onClick={() => {
+                                setCurrentCategory(category)
+                            }}
+                            >
                                 {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
